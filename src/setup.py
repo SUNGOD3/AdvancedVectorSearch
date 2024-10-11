5ef744aae517f655e588e88b233a9731728ac178
TAI-YANG <72863539+SUNGOD3@users.noreply.github.com>
2024-10-11 08:20:13 +0000

    clean

--- src/setup.py
+++ src/setup.py
@@ -3,15 +3,15 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 ext_modules = [
     Pybind11Extension(
-        "advanced_search_cpp",
-        ["advanced_search_module.cpp", "advanced_search.cpp"],
+        "src/advanced_search_cpp",
+        ["src/advanced_search_module.cpp", "src/advanced_search.cpp"],
         include_dirs=[".", "<path_to_pybind11_include>"],
         extra_compile_args=['-std=c++11'],
     ),
 ]
 
 setup(
-    name="advanced_search_cpp",
+    name="src/advanced_search_cpp",
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
 )
